b0f81e8a56d1a9d86bc30bb2be10d77c4c95b6ea
r.galimov <samtakoy.first@gmail.com>
2020-09-30 11:54:09 +0300

    Optimize imports

--- app/src/main/java/ru/samtakot/rttest/presentation/settings/SettingsFragment.kt
+++ app/src/main/java/ru/samtakot/rttest/presentation/settings/SettingsFragment.kt
@@ -1,17 +1,11 @@
 package ru.samtakot.rttest.presentation.settings
 
-import android.os.AsyncTask
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.Toast
 import androidx.preference.Preference
 import com.bumptech.glide.Glide
 import io.reactivex.Completable
 import moxy.ktx.moxyPresenter
-import moxy.viewstate.strategy.OneExecutionStateStrategy
-import moxy.viewstate.strategy.StateStrategyType
 import ru.samtakot.rttest.R
 import ru.samtakot.rttest.app.di.Di
 import ru.samtakot.rttest.extensions.ioToMain
